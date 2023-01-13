10a8b08fadd71b7f33fd965c626c467a07daf816
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-13 08:36:00 +0530

    initial commit

--- main.js
+++ main.js
@@ -7,31 +7,62 @@ const getUser = async (username) => {
           if (res.status == 200) {
                const data = await res.json();
                return data;
-          }
-          else
-          {
-                    dispalyError(res.status);
-
+          } else {
+               dispalyError(res.status);
           }
      } catch (error) {
           console.log(error);
      }
 };
-
-const dispalyError=(status)=>{
-     const msg=document.querySelector('#message');
-     var errormsg="";
-     if(status ===404)
-     {
-          errormsg=`<div class="alert alert-danger text-center">User not found</div>`;
+const loadData = (data) => {
+     var userdata = "";
+     userdata += ` <div class="profile-userpic">
+     <img src="${data.avatar_url}" class="d-block">
+ </div>
+ <div class="profile-usertitle">`;
+ if(data.name!==null) {
+ userdata+= `<div class="profile-usertitle-name">${data.name}</div>`;
+ }
+ userdata+=  `<div class="profile-usertitle-job">${data.login}</div>
+ </div>   
+<div class="portlet light bordered">
+ <!-- STAT -->
+ <div class="row list-separated profile-stat">
+     <div class="col-md-6 col-sm-6 col-xs-6">
+         <div class="uppercase profile-stat-title">${data.followers}</div>
+         <div class="uppercase profile-stat-text"> Followers </div>
+     </div>
+     <div class="col-md-6 col-sm-6 col-xs-6">
+         <div class="uppercase profile-stat-title">${data.following}</div>
+         <div class="uppercase profile-stat-text"> Following </div>
+     </div>
+ </div>
+ <!-- END STAT --><div>`;
+  if(data.bio!==null){
+  userdata+=`<h4 class="profile-desc-title">About ${data.name}</h4>
+     <span class="profile-desc-text"> ${data.bio}</span>`;
+  }
+  if(data.twitter_username!==null){
+  userdata+=`</div><div class="margin-top-20 profile-desc-link">
+ <i class="fab fa-twitter"></i>
+     <a target="_blank" href="https://www.twitter.com/${data.twitter_username}">@${data.twitter_username}</a>
+ </div></div>`
+ };
+ document.querySelector('#profile').innerHTML =userdata;
+};
+const dispalyError = (status) => {
+     const msg = document.querySelector("#message");
+     var errormsg = "";
+     if (status === 404) {
+          errormsg = `<div class="alert alert-danger text-center">User not found</div>`;
+     } else {
+          errormsg = `<div class="alert alert-danger text-center">Something went wrong</div>`;
      }
-     else
-     {
-          errormsg=`<div class="alert alert-danger text-center">Something went wrong</div>`;   
-     }
-     msg.innerHTML=errormsg;
-     setTimeout(()=>{msg.innerHTML=``},5000);
-}
+     msg.innerHTML = errormsg;
+     setTimeout(() => {
+          msg.innerHTML = ``;
+     }, 5000);
+};
 
 const doc = document.addEventListener("DOMContentLoaded", () => {
      const searchForm = document.querySelector("#searchForm");
@@ -41,7 +72,13 @@ const doc = document.addEventListener("DOMContentLoaded", () => {
           const username = searchInput.value.trim();
           if (username.length > 0) {
                const user = await getUser(username);
-               console.log(user);
+               if (user.login) {
+                    loadData(user);
+                    document.querySelector(".searchblock").style.display =
+                         "none";
+                    document.querySelector(".profile").style.display = "block";
+                    console.log(user);
+               }
           }
      });
 });
